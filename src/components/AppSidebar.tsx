09a7c4d7b2f7edfbe2f26caf717eea3051573f94
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-04-03 16:27:57 -0500

    Full Stack Next.js and Supabase. 0:58:35 Build Sidebar
    - Instalacion del `sidebar` del sitio `ui.shadcn.com`.
    - Creamos el componente  **`AppSidebar.tsx`** con lo que mostraría de las notas.
    - Creamos el componente  **`SideGroupContent.tsx`** para que reciba las `notes`.
    
    #11_Build_Sidebar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,61 @@
+import { getUser } from "@/auth/server";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  // SidebarGroupContent,
+  SidebarGroupLabel,
+  // SidebarHeader,
+} from "@/components/ui/sidebar";
+import { prisma } from "@/db/prisma";
+import { Note } from "@prisma/client";
+import Link from "next/link";
+import SidebarGroupContent from "./SidebarGroupContent";
+
+async function AppSidebar() {
+  const user = await getUser(); // "@/auth/server"
+
+  let notes: Note[] = []; // "@prisma/client"
+
+  if (user) {
+    // Conditicional para verificar si el usuario existe
+    // Query para traer las notas del usuario
+    notes = await prisma.note.findMany({
+      // "@/db/prisma",
+      where: {
+        authorId: user.id,
+      },
+      orderBy: {
+        updateAt: "desc",
+      },
+    });
+  }
+
+  return (
+    <Sidebar>
+      {/*<SidebarHeader />*/}
+      <SidebarContent className="custom-scrollbar">
+        <SidebarGroup>
+          <SidebarGroupLabel className="mt-2 mb-2 text-lg">
+            {user ? (
+              "Your Notes"
+            ) : (
+              <p>
+                <Link href="/login" className="underline">
+                  Login
+                </Link>{" "}
+                to see your notes
+              </p>
+            )}
+          </SidebarGroupLabel>
+          {user && <SidebarGroupContent notes={notes} />}
+        </SidebarGroup>
+        {/*<SidebarGroup />*/}
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
