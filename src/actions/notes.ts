78d9efa40eb95caaafb7365a57c59280709a7244
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-04-12 19:34:49 -0500

    Full Stack Next.js and Supabase. 1:29:23 Finish Sidebar. Part 2
    - Completamos componentes **`SelectNoteButton.tsx`** y **`DeleteNoteButton.tsx`**.
    - se añade acción en  **`actions/note.ts`**.
    - Ubicación correcta del  componente **`DeleteNoteButton.tsx`** .
    
    18_Finish_Sidebar/Part2

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -9,12 +9,13 @@ export async function createNoteAction(noteId: string) {
     const user = await getUser(); // "@/auth/server"
     if (!user) throw new Error("You must be logged in to update a note");
 
-    await prisma.note.create({ // "@/db/prisma"
-      data: { 
-        id: noteId, 
-        authorId: user.id, 
+    await prisma.note.create({
+      // "@/db/prisma"
+      data: {
+        id: noteId,
+        authorId: user.id,
         text: "", // Assuming text is an empty string initially
-      }, 
+      },
     });
 
     return { errorMessage: null };
@@ -37,5 +38,20 @@ export async function updateNoteAction(noteId: string, text: string) {
     return { errorMessage: null };
   } catch (error) {
     return handleError(error); // "@/lib/utils"
+  }
+}
+
+export async function deleteNoteAction(noteId: string) {
+  try {
+    const user = await getUser();
+    if (!user) throw new Error("You must be logged in to delete a note");
+
+    await prisma.note.delete({
+      where: { id: noteId, authorId: user.id },
+    });
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
   }
 }
